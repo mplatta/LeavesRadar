9ab4193e01d9cc3387962fe86a0ac6d8a885f319
mplatta <m.platta@wp.pl>
2019-07-21 19:11:45 +0200

    first step to multithread, and read all images in file

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,97 @@
+#include <iostream>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #include "cartographer.hpp"
 #include "formatted_log.hpp"
 
 using namespace cv;
 
+#define NUM_THREADS 4
+
+static bool dirExists(const char *path)
+{
+	struct stat info;
+
+	if( stat( path, &info ) != 0 )
+    	formatted_err( "Cannot access %s", path );
+	else if( info.st_mode & S_IFDIR )
+	{
+    	formatted_log( "%s directory exists", path );
+
+    	return true;
+	}
+	else
+    	formatted_err( "%s is no directory", path );
+	
+	return false;
+}
+
 int main( int argc, char** argv ) 
 {
-	// for tests
-	formatted_log("aaa%d", 2);
-	formatted_log("aaa");
+	pthread_t threads[NUM_THREADS];
+	string file = "";
 
-	Cartographer *cartographer = new Cartographer();
-
-	Mat image, src_gray;
-	Mat dst, detected_edges;
-
-	image = imread("../res/oak.jpg", CV_LOAD_IMAGE_COLOR);
-
-	if(image.empty()) 
+	if (argc > 1) 
 	{
-		std::cout <<  "Could not open or find the image" << std::endl;
-		return -1;
+		for (int i = 0; i < argc; ++i) 
+		{
+			if ( (strcmp(argv[i], "--file") == 0 ) &&
+				 (argc >= i + 1 ) )
+			{
+				file = argv[i + 1];
+			}
+
+			if ( strcmp(argv[i], "-g") ) {
+				// TODO add flag to run in graphic mode
+				;
+			}
+		}
 	}
 
-	cartographer->setSrcImg(image);
-	dst = cartographer->getBordered(false);
+	// tmp
+	if (file == "")
+	{
+		Cartographer *cartographer = new Cartographer();
 
-	namedWindow( "Display window", cv::WINDOW_AUTOSIZE );
+		Mat image, src_gray;
+		Mat dst, detected_edges;
 
-	imshow( "Display window", dst);
+		image = imread("../res/oak.jpg", CV_LOAD_IMAGE_COLOR);
+
+		if(image.empty()) 
+		{
+			std::cout <<  "Could not open or find the image" << std::endl;
+			return -1;
+		}
+
+		cartographer->setSrcImg(image);
+		dst = cartographer->getBordered(false);
+
+		namedWindow( "Display window", cv::WINDOW_AUTOSIZE );
+
+		imshow( "Display window", dst);
 	
-	waitKey(0);
+		waitKey(0);
 
-	delete cartographer;
+		delete cartographer;
+	}
+	else 
+	{
+		std::vector<string> image_names;
+
+		if (dirExists(file.c_str()))
+		{
+			glob(file + "/*.jpg", image_names);
+
+			for (size_t i = 0; i < image_names.size(); ++i)
+			{
+				printf("%s\n", image_names[i].c_str());
+			}
+		}
+
+	}
 	
 	return 0;
 }
