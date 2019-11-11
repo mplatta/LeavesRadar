5e34582f27bd84870778446c413a78035750409c
klata <m.platta@Wp.pl>
2019-11-11 17:05:54 +0100

    fix memory dump

--- src/mgr/starting_point.cpp
+++ src/mgr/starting_point.cpp
@@ -1,90 +1,104 @@
 #include "starting_point.hpp"
 
 void StartingPoint::setImgBin(cv::Mat img){
-    // formatted_log("Size this [%d, %d]", this->img.rows, this->img.cols);
-    // formatted_log("Size img [%d, %d]", img.rows, img.cols);
+	// formatted_log("Size this [%d, %d]", this->img.rows, this->img.cols);
+	// formatted_log("Size img [%d, %d]", img.rows, img.cols);
 
 	cv::Mat img_gray, temp;
 	img.convertTo(temp, CV_8U);
 	cvtColor(temp, img_gray, CV_BGR2GRAY);
 	threshold(img_gray, this->img, 200.0, 255.0, CV_THRESH_BINARY);    
 
-    // formatted_log("Size this 2 [%d, %d]", this->img.rows, this->img.cols);
+	// formatted_log("Size this 2 [%d, %d]", this->img.rows, this->img.cols);
 }
 
 cv::Point2f StartingPoint::getStartingPoint(double percent){
 	cv::Point2f a(0.0f, 0.0f), b(0.0f, 0.0f);    
-    int rows = this->img.rows;
-    int cols = this->img.cols;
+	int rows = this->img.rows;
+	int cols = this->img.cols;
 
-    formatted_log("Size [%d, %d]", rows, cols);
+	formatted_log("Size [%d, %d]", rows, cols);
 
-    cv::Point2f check = getPointOnStraightX(this->str, 0.0);
+	cv::Point2f check;
 
-    if(check.y >= 0 && check.y <= rows){
-        // formatted_log("if true");
-formatted_log("T1");
-        for(int i = cols; i >= 0; i--){
-            cv::Point2f p = getPointOnStraightX(this->str, (double)i);
+	if(this->str.coeff.b == 0){
+		check = getPointOnStraightY(this->str, 0.0);
+	}
+	else{
+		check = getPointOnStraightX(this->str, 0.0);
+	}
 
-            // formatted_log("loop1 %d", i);
-            // formatted_log("Point p [%f, %f]", p.y, p.x);
-            // formatted_log("this->img.at<uchar>(p) %d", this->img.at<uchar>(p));
-            
-            if(this->img.at<uchar>(p) == 0 && p.y >= 0 && p.x >= 0 && p.y <= rows && p.x <= cols){
-                b = p;
-                break;
-            }
-        }    
+	if(check.y >= 0 && check.y <= rows) {
+		// formatted_log("if true");
 
-        for(int i = 0; i < cols; i++){
-            cv::Point2f p = getPointOnStraightX(this->str, (double)i);
+		for(int i = cols; i >= 0; i--) {
+			cv::Point2f p = getPointOnStraightX(this->str, (double)i);
 
-            // formatted_log("loop2 %d", i);
-            // formatted_log("Point p [%f, %f]", p.y, p.x);
-            // formatted_log("this->img.at<uchar>(p) %d", this->img.at<uchar>(p));
+			// formatted_log("loop1 %d", i);
+			// formatted_log("Point p [%f, %f]", p.y, p.x);
+			// formatted_log("this->img.at<uchar>(p) %d", this->img.at<uchar>(p));
+			if(p.y > 0 && p.x > 0 && p.y < rows && p.x < cols)
+			{
+				if(this->img.at<uchar>(p) == 0) {
+					b = p;
+					break;
+				}
+			}
+		}    
 
-            if(this->img.at<uchar>(p) == 0 && p.y >= 0 && p.x >= 0 && p.y <= rows && p.x <= cols){
-                a = p;
-                break;
-            }
-        }        
-    }else{
-        formatted_log("T2");
-        // formatted_log("if false");
+		for(int i = 0; i < cols; i++){
+			cv::Point2f p = getPointOnStraightX(this->str, (double)i);
 
-        for(int i = rows; i >= 0; i--){
-            cv::Point2f p = getPointOnStraightY(this->str, (double)i);
-        formatted_log("T4");
+			// formatted_log("loop2 %d", i);
+			// formatted_log("Point p [%f, %f]", p.y, p.x);
+			// formatted_log("this->img.at<uchar>(p) %d", this->img.at<uchar>(p));
+			if(p.y > 0 && p.x > 0 && p.y < rows && p.x < cols)
+			{
+				if(this->img.at<uchar>(p) == 0) {
+					a = p;
+					break;
+				}
+			}
+		}        
+	} else {
+		// formatted_log("if false");
 
-            // formatted_log("loop1 %d", i);
-            // formatted_log("Point p [%f, %f]", p.y, p.x);
-            // formatted_log("this->img.at<uchar>(p) %d", this->img.at<uchar>(p));
-            
-            if(this->img.at<uchar>(p) == 0 && p.y >= 0 && p.x >= 0 && p.y <= rows && p.x <= cols){
-                b = p;
-                break;
-            }
-        }    
+		for(int i = rows; i >= 0; i--){
+			cv::Point2f p = getPointOnStraightY(this->str, (double)i);
 
-        for(int i = 0; i < rows; i++){
-        formatted_log("T5");
-        	cv::Point2f p = getPointOnStraightY(this->str, (double)i);
+			// formatted_log("loop1 %d", i);
+			// formatted_log("Point p [%f, %f]", p.y, p.x);
+			// formatted_err("TTTTTTTTTTTTTTTTUUUUUUUUUUUUU %d, %d:%d", this->img.empty(), this->img.size().width, this->img.size().height);
+			// // formatted_log("this->img.at<uchar>(p) %d", this->img.at<uchar>(p));
+			// formatted_err("ssssssssssssssssssssssssssssssssss: %d", this->img.type());
+			if(p.y > 0 && p.x > 0 && p.y < rows && p.x < cols) {
+				if(this->img.at<uchar>(p) == 0) {
+					b = p;
+					break;
+				}
+			}
+		}    
 
-            // formatted_log("loop2 %d", i);
-            // formatted_log("Point p [%f, %f]", p.y, p.x);
-            // formatted_log("this->img.at<uchar>(p) %d", this->img.at<uchar>(p));
+		for(int i = 0; i < rows; i++){
+			// formatted_err("22222222222222222: %d", this->img.type());
+			cv::Point2f p = getPointOnStraightY(this->str, (double)i);
 
-        	if(this->img.at<uchar>(p) == 0 && p.y >= 0 && p.x >= 0 && p.y <= rows && p.x <= cols){
-        	    a = p;
-        	    break;
-        	}
-        }
-    }
-    formatted_log("T3");
-    // formatted_log("Point a [%f, %f]", a.y, a.x);
-    // formatted_log("Point b [%f, %f]", b.y, b.x);
+			// formatted_log("loop2 %d", i);
+			// formatted_log("Point p [%f, %f]", p.y, p.x);
+			// formatted_log("this->img.at<uchar>(p) %d", this->img.at<uchar>(p));
+			if(p.y > 0 && p.x > 0 && p.y < rows && p.x < cols) {
+				if(this->img.at<uchar>(p) == 0) {
+					a = p;
+					break;
+				}
+			}
+		}
+	}
 
-    double ysp = ((b.y - a.y) * percent) + a.y;
-    return getPointOnStraightY(this->str, ysp);
+			// formatted_err("111111111111111: %d", this->img.type());
+	// formatted_log("Point a [%f, %f]", a.y, a.x);
+	// formatted_log("Point b [%f, %f]", b.y, b.x);
+
+	double ysp = ((b.y - a.y) * percent) + a.y;
+	return getPointOnStraightY(this->str, ysp);
 }
